092dba5a1c1c009cef365fe4730a5e26806de40a
PrincessDao <grprincessdao@gmail.com>
2025-02-10 23:57:51 +0700

    overwriting ScreenSizeProvider (android)

--- composeApp/src/androidMain/kotlin/org/example/kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/kmp/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import android.content.Context
-import android.os.Build
-import android.util.DisplayMetrics
-import androidx.annotation.RequiresApi
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity(), PlatformController {
@@ -35,14 +31,4 @@ fun AppAndroidPreview() {
     App(object : PlatformController {
         override fun setStatusBarColor(color: Int) {}
     })
-}
-
-class AndroidScreenSizeProvider(private val context: Context): ScreenSizeProvider {
-    @RequiresApi(Build.VERSION_CODES.R)
-    override fun getScreenWidth(): Float {
-        val displayMetrics = DisplayMetrics()
-        val display = context.display
-        display.getRealMetrics(displayMetrics)
-        return displayMetrics.widthPixels.toFloat()
-    }
 }
--- composeApp/src/iosMain/kotlin/org/example/kmp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/kmp/MainViewController.kt
@@ -4,10 +4,3 @@ import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIScreen
 
 fun MainViewController() = ComposeUIViewController { App() }
-
-class IosScreenSizeProvider: ScreenSizeProvider {
-    override fun getScreenWidth(): Float {
-        val screen = UIScreen.mainScreen
-        return screen.bounds.size.width.toFloat()
-    }
-}
