08b6a66706b6e2412555320cd6748ba9f899f054
PrincessDao <grprincessdao@gmail.com>
2025-02-11 00:23:47 +0700

    overwriting file ios

--- composeApp/src/iosMain/kotlin/org/example/kmp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/kmp/MainViewController.kt
@@ -2,5 +2,36 @@ package org.example.kmp
 
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIScreen
+import platform.UIKit.UIApplication
+import platform.UIKit.UIStatusBarManager
+import platform.UIKit.UIWindow
+import platform.UIKit.UIViewController
+import platform.UIKit.UIColor
+import platform.darwin.NSObject
 
-fun MainViewController() = ComposeUIViewController { App() }
+class IOSPlatformController(private val window: UIWindow?) : PlatformController {
+    override fun setStatusBarColor(color: Int) {
+        val uiColor = UIColor(
+            red = ((color shr 16) and 0xFF).toDouble() / 255.0,
+            green = ((color shr 8) and 0xFF).toDouble() / 255.0,
+            blue = (color and 0xFF).toDouble() / 255.0,
+            alpha = 1.0
+        )
+        window?.rootViewController?.view?.backgroundColor = uiColor
+    }
+}
+
+class IOSScreenSizeProvider : ScreenSizeProvider {
+    override fun getScreenWidth(): Float {
+        return UIScreen.mainScreen.bounds.size.width.toFloat()
+    }
+}
+
+actual fun getScreenSizeProvider(): ScreenSizeProvider {
+    return IOSScreenSizeProvider()
+}
+
+fun MainViewController() = ComposeUIViewController {
+    val platformController = IOSPlatformController(UIApplication.sharedApplication.keyWindow)
+    App(platformController)
+}
