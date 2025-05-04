023cee1170dddd564b7d1a55f112ed063c00e83e
PrincessDao <grprincessdao@gmail.com>
2025-05-05 03:17:44 +0700

    add framework files

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,9 +2,67 @@ import UIKit
 import SwiftUI
 import ComposeApp
 
+func DotBanner(totalDots: Int, currentIndex: Int, selectedColor: UIColor, unselectedColor: UIColor) -> UIViewController {
+    MainViewControllerKt.dotBannerViewController(
+        totalDots: Int32(totalDots),
+        currentIndex: Int32(currentIndex),
+        unselectedColorHex: unselectedColor.toHex(),
+        selectedColorHex: selectedColor.toHex()
+    )
+}
+
+func DotPersonalOffer(totalDots: Int, selectedColor: UIColor, unselectedColor: UIColor) -> UIViewController {
+    MainViewControllerKt.dotPersonalOfferViewController(
+        totalDots: Int32(totalDots),
+        unselectedColorHex: unselectedColor.toHex(),
+        selectedColorHex: selectedColor.toHex()
+    )
+}
+
+extension UIColor {
+    func toHex() -> String {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        let rgb = (Int)(red * 255)<<16 | (Int)(green * 255)<<8 | (Int)(blue * 255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
+
+
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+        let vc1 = DotBanner(totalDots: 7, currentIndex: 2, selectedColor: .red, unselectedColor: .gray)
+        let vc2 = DotPersonalOffer(totalDots: 7, selectedColor: .green, unselectedColor: .gray)
+        let stack = UIStackView()
+         stack.axis = .vertical
+         stack.distribution = .fillEqually
+         stack.translatesAutoresizingMaskIntoConstraints = false
+
+         stack.addArrangedSubview(vc1.view)
+         stack.addArrangedSubview(vc2.view)
+
+         let container = UIViewController()
+         container.addChild(vc1)
+         container.addChild(vc2)
+
+         container.view.addSubview(stack)
+
+         NSLayoutConstraint.activate([
+             stack.topAnchor.constraint(equalTo: container.view.topAnchor),
+             stack.leadingAnchor.constraint(equalTo: container.view.leadingAnchor),
+             stack.trailingAnchor.constraint(equalTo: container.view.trailingAnchor),
+             stack.bottomAnchor.constraint(equalTo: container.view.bottomAnchor)
+         ])
+
+         vc1.didMove(toParent: container)
+         vc2.didMove(toParent: container)
+
+         return container
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
--- kmp/src/iosMain/kotlin/com/example/kmp/FunctionsForIos.kt
+++ kmp/src/iosMain/kotlin/com/example/kmp/FunctionsForIos.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalObjCName::class)
+
 package com.example.kmp
 
 import androidx.compose.ui.graphics.Color
@@ -7,68 +9,53 @@ import androidx.compose.ui.window.ComposeUIViewController
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.UIKit.UIViewController
 import kotlin.experimental.ExperimentalNativeApi
+import kotlin.experimental.ExperimentalObjCName
 
-internal fun initFramework() {
-}
+fun parseColorHex(hex: String): Color {
+    val cleanHex = hex.removePrefix("#")
+    val colorLong = cleanHex.toLong(16)
 
-@OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
-@CName("callComposableFunction")
-fun callComposableFunction(
-    functionName: String,
-    args: Map<String, Any> = emptyMap()
-): UIViewController {
-    println("Calling composable function: $functionName with args: $args")
-
-    return ComposeUIViewController {
-        when (functionName) {
-            "Padding" -> {
-                val width = (args["width"] as? Number)?.toFloat()?.dp ?: DefaultPaddingWidth.current
-                val height = (args["height"] as? Number)?.toFloat()?.dp ?: DefaultPaddingHeight.current
-                val backgroundColor = when (val color = args["color"]) {
-                    is String -> parseColor(color)
-                    else -> DefaultPaddingBackgroundColor.current
-                }
-                val visibility = (args["alpha"] as? Number)?.toFloat() ?: DefaultPaddingVisibility.current
-
-                println("Padding params -> width: $width, height: $height, color: $backgroundColor, alpha: $visibility")
-
-                ComposeFunctions.Padding(
-                    width = width,
-                    height = height,
-                    backgroundColor = backgroundColor,
-                    visibility = visibility
-                )
-            }
-            "Button" -> {
-                val text = args["text"] as? String ?: "Default"
-                val color = when (val color = args["color"]) {
-                    is String -> parseColor(color)
-                    else -> DefaultPaddingBackgroundColor.current
-                }
-
-                println("Button params -> text: $text, color: $color")
-
-                ComposeFunctions.Button(
-                    text = text,
-                    color = color
-                )
-            }
-            else -> {
-                println("Unknown function: $functionName")
-                error("Unknown function: $functionName")
-            }
+    return when (cleanHex.length) {
+        6 -> { // RRGGBB
+            val r = ((colorLong shr 16) and 0xFF).toInt()
+            val g = ((colorLong shr 8) and 0xFF).toInt()
+            val b = (colorLong and 0xFF).toInt()
+            Color(red = r / 255f, green = g / 255f, blue = b / 255f)
         }
+        8 -> { // AARRGGBB
+            val a = ((colorLong shr 24) and 0xFF).toInt()
+            val r = ((colorLong shr 16) and 0xFF).toInt()
+            val g = ((colorLong shr 8) and 0xFF).toInt()
+            val b = (colorLong and 0xFF).toInt()
+            Color(red = r / 255f, green = g / 255f, blue = b / 255f, alpha = a / 255f)
+        }
+        else -> throw IllegalArgumentException("Invalid color hex: $hex")
     }
 }
 
-fun parseColor(color: String): Color {
-    return when (color.lowercase()) {
-        "red" -> Color.Red
-        "green" -> Color.Green
-        "blue" -> Color.Blue
-        "black" -> Color.Black
-        "white" -> Color.White
-        "gray" -> Color.Gray
-        else -> Color.Gray
-    }
+@ObjCName("dotPersonalOfferViewController")
+fun DotPersonalOfferViewController(
+    totalDots: Int = 5,
+    unselectedColorHex: String = "#СССССС",
+    selectedColorHex: String = "#FF00000"
+): UIViewController = ComposeUIViewController {
+    DotPersonalOffer(
+        totalDots = totalDots,
+        unselectedColor = parseColorHex(unselectedColorHex),
+        selectedColor = parseColorHex(selectedColorHex)
+
+    )
 }
+@ObjCName("dotBannerViewController")
+fun DotBannerViewController(
+    totalDots: Int = 5,
+    currentIndex: Int = 0,
+    unselectedColorHex: String = "#СССССС",
+    selectedColorHex: String = "#FF00000"
+): UIViewController = ComposeUIViewController {
+    DotBanner(
+        totalDots = totalDots,
+        currentIndex = currentIndex,
+        unselectedColor = parseColorHex(unselectedColorHex),
+        selectedColor = parseColorHex(selectedColorHex)
+    ) }
