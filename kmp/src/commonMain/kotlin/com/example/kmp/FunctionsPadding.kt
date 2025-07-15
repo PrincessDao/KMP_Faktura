8ac7cd5aadda912455cc1225f2fa7085cc6533c0
PrincessDao <grprincessdao@gmail.com>
2025-07-15 21:00:22 +0700

    add flutter UI

--- kmp/src/commonMain/kotlin/com/example/kmp/FunctionsPadding.kt
+++ kmp/src/commonMain/kotlin/com/example/kmp/FunctionsPadding.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 
-    @Composable
+@Composable
     fun Padding(
         width: Dp = DefaultPaddingWidth.current,
         height: Dp = DefaultPaddingHeight.current,
